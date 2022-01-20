59e7f4e84c078318acfb0f7db12b9b34f9b55609
Tamal Saha <tamal@appscode.com>
2022-01-20 09:39:39 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -2,16 +2,10 @@ package main
 
 import (
 	"context"
-	"fmt"
-	//	"time"
 
-	// 	"github.com/nats-io/nats.go"
-	// 	"github.com/tamalsaha/nats-hop-demo/shared"
-	// 	"github.com/tamalsaha/nats-hop-demo/transport"
 	core "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	//	"k8s.io/klog/v2"
 	"k8s.io/klog/v2/klogr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -21,7 +15,6 @@ import (
 func NewClient() (client.Client, error) {
 	scheme := runtime.NewScheme()
 	_ = clientgoscheme.AddToScheme(scheme)
-	crdinstall.Install(scheme)
 
 	ctrl.SetLogger(klogr.New())
 	cfg := ctrl.GetConfigOrDie()
@@ -43,62 +36,15 @@ func NewClient() (client.Client, error) {
 	})
 }
 
-var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
-)
-
 func main() {
-	if err := run(); err != nil {
-		panic(err)
-	}
-}
-
-func run() error {
-	_ = clientgoscheme.AddToScheme(scheme)
-
-	// 	nc, err := nats.Connect(shared.NATS_URL)
-	// 	if err != nil {
-	// 		klog.Fatalln(err)
-	// 	}
-	// 	defer nc.Close()
-
-	ctrl.SetLogger(klogr.New())
-	cfg := ctrl.GetConfigOrDie()
-
-	// 	tr, err := cfg.TransportConfig()
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	cfg.Transport, err = transport.New(tr, nc, "k8s", 10000*time.Second)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-
-	mapper, err := apiutil.NewDynamicRESTMapper(cfg)
-	if err != nil {
-		return err
-	}
-
-	c, err := client.New(cfg, client.Options{
-		Scheme: scheme,
-		Mapper: mapper,
-		Opts: client.WarningHandlerOptions{
-			SuppressWarnings:   false,
-			AllowDuplicateLogs: false,
-		},
-	})
-	if err != nil {
-		return err
-	}
-
-	var nodes core.NodeList
-	err = c.List(context.TODO(), &nodes)
+	c, err := NewClient()
 	if err != nil {
 		panic(err)
 	}
-	for _, n := range nodes.Items {
-		fmt.Println(n.Name)
+
+	var cms core.ConfigMapList
+	err = c.List(context.TODO(), &cms)
+	if err != nil {
+		panic(err)
 	}
-	return nil
 }
